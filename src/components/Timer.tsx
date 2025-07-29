7cebbb7981a219ec2492bf325cf42ebc3cfbd516
Allen Thomas <aabmtho12@gmail.com>
2025-07-29 11:03:19 +0200

    cleaned up the timer looks, coding on a train lowkey horrible, this part is FINISHED

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTimer } from "@/hooks/useTimer";
+import { time } from "console";
 
 interface TimerProps {
     onTimerFinish?: () => void 
 }
 
 export const Timer : React.FC<TimerProps> = ({onTimerFinish}) => {
-    const {formattedTime, isFinished} = useTimer(3)
-
+    const {formattedTime, isFinished, timeLeft} = useTimer(3)
+    const [isRed, setIsRed] = useState(false)
+    //setTimeout (() => setIsRed(true), 10)
+    useEffect(() => {
+        if (timeLeft <= 1.1){
+            setTimeout(() => setIsRed(!isRed), 200 )
+        }
+    }, [isRed, setIsRed, timeLeft])
     useEffect(() => {
         if (isFinished && onTimerFinish){
             onTimerFinish()
@@ -15,8 +22,8 @@ export const Timer : React.FC<TimerProps> = ({onTimerFinish}) => {
     }, [isFinished, onTimerFinish])
 
     return(
-        <div>
-            <h1> {formattedTime} </h1>
+        <div className = "">
+            <h1 className = {`${(isRed ) ? "text-red-600" : ""} transition-none text-4xl mt-8`} > {formattedTime} {isRed}</h1>
         </div>
     )
 }
